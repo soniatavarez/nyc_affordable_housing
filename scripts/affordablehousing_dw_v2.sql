2baad716649a4b261dbcfa0a9d612cd00cb95eb3
soniatavarez <sonia.tavarez@baruchmail.cuny.edu>
2024-05-02 17:22:09 -0400

    updates to dw

--- scripts/affordablehousing_dw_v2.sql
+++ scripts/affordablehousing_dw_v2.sql
@@ -1,12 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS "instance";
 
-CREATE  TABLE "instance".dim_boroughtype (
+CREATE  TABLE "instance".dim_boroughtype ( 
 	boroughtype_id       integer  NOT NULL  ,
 	borough_name         varchar(100)    ,
 	CONSTRAINT pk_borough PRIMARY KEY ( boroughtype_id )
  );
 
-CREATE  TABLE "instance".dim_buildingtype (
+CREATE  TABLE "instance".dim_buildingtype ( 
 	buildingtype_id      integer  NOT NULL  ,
 	house_number         integer    ,
 	street_name          varchar(100)    ,
@@ -22,17 +22,17 @@ CREATE  TABLE "instance".dim_buildingtype (
 	CONSTRAINT pk_dim_buildingtype PRIMARY KEY ( buildingtype_id )
  );
 
-CREATE  TABLE "instance".dim_projecttype (
+CREATE  TABLE "instance".dim_projecttype ( 
 	projecttype_id       integer  NOT NULL  ,
 	project_name         varchar(100)    ,
 	project_id           integer    ,
 	CONSTRAINT pk_project PRIMARY KEY ( projecttype_id )
  );
 
-CREATE  TABLE "instance".entity (
+CREATE  TABLE "instance".entity ( 
  );
 
-CREATE  TABLE "instance".facts_housing (
+CREATE  TABLE "instance".facts_housing ( 
 	fact_id              integer  NOT NULL  ,
 	extremely_low_income_units integer    ,
 	very_low_income_units integer    ,
